465badde52623078cb2cabe5cf2e7510202962cb
sidrarasool <sidra.rasool30@gmail.com>
2021-11-07 20:23:10 +0500

    second commit

--- app/src/main/java/com/example/task2/MainActivity.java
+++ app/src/main/java/com/example/task2/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.task2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void practice(View view) {
+        intent = new Intent(MainActivity.this, practiceActivity.class);
+        startActivity(intent);
+    }
+
 }
